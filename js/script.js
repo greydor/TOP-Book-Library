3287683db061eee6a2b97d872a556977a6d37365
greydor <ghub@us.jabbermail.com>
2023-01-17 12:41:35 -0500

    wip

--- js/script.js
+++ js/script.js
@@ -8,20 +8,34 @@ function Book(title, author, pages, hasRead) {
 }
 
 Book.prototype.info = function bookInfo() {
-  let readStatement;  
+  let readStatement;
   if (this.hasRead === true) {
-
-    readStatement = "has been read"
+    readStatement = "has been read";
+  } else {
+    readStatement = "not read yet";
   }
-  else {
-    readStatement = "not read yet"
-  }
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatement}`
-}
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatement}`;
+};
 
 myLibrary.push(new Book("Game of Thrones", "George R.R. Martin", 800, true));
 console.log(myLibrary[0].info());
 
-function addBookToLibrary() {
-  // do stuff here
+const form = document.querySelector("form");
+const addBook = document.querySelector("#add-book");
+addBook.addEventListener("click", () => {
+  form.classList.add("visible");
+});
+
+form.addEventListener("submit", (e) => {
+  form.classList.remove("visible");
+  const author = form.author.value;
+  const title = form.title.value;
+  const pages = form.pages.value;
+  const bookInfo = {author, title, pages};
+  addBookToLibrary(bookInfo);
+  e.preventDefault();
+});
+
+function addBookToLibrary(bookInfo) {
+
 }
